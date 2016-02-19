61bc861d38248c47b102f97c7a4aeb17212fe1a9
Owenhong <520UED.com@gmail.com>
2016-02-19 18:10:32 +0800

    updata更新
    updata更新

--- updata/updata.js
+++ updata/updata.js
@@ -20,31 +20,8 @@ var copyPath, execPath;
 
 
 // Args passed when new app is launched from temp dir during update
-if(gui.App.argv.length) {
-    console.log("abc");
-    // ------------- Step 5 -------------
-    copyPath = gui.App.argv[0];
-    execPath = gui.App.argv[1];
-
-    console.log(copyPath);
-    console.log(execPath);
-
-    // Replace old app, Run updated app from original location and close temp instance
-    upd.install(copyPath, function(err) {
-
-        console.log(err);
-
-        if(!err) {
-
-            // ------------- Step 6 -------------
-            upd.run(execPath, null);
-            gui.App.quit();
-        }
-    });
-}else { // if no arguments were passed to the app
-
-    console.log("ued");
-
+console.log(gui.App.argv.length);
+if(!gui.App.argv.length) {
     // ------------- Step 1 -------------
     upd.checkNewVersion(function(error, newVersionExists, manifest) {
         if(error){
@@ -69,6 +46,13 @@ if(gui.App.argv.length) {
                         console.log(newAppPath);
                         console.log(error);
 
+                        //解压完后删除更新包
+                        fse.remove(filename, function(err){
+                            if (err) return console.error(err);
+
+                            console.log("zip delet success!")
+                        });
+
                         if (!error) {
                             console.log("runInstaller...");
                             var $dirPath = path.dirname(newAppPath);
@@ -78,27 +62,17 @@ if(gui.App.argv.length) {
 
                             //拷贝文件
                             fse.copy($dirPath, $getAppPath, function (err) {
-                                if (err) return console.error(err)
-                                console.log('copy success!')
+                                if (err) return console.error(err);
 
-                                //更新成功文件后删除临时目�?
-                                //fse.remove($dirPath, function (err) {
-                                //    if (err){
-                                //        gui.App.quit();
-                                //    }
-                                //
-                                //    //gui.App.quit();
-                                //});
+                                //更新成功文件后删除临时目
+                                fse.remove($dirPath, function (err) {
+                                    if (err){
+                                        gui.App.quit();
+                                    }
+                                    console.log("updata success!")
+                                    //gui.App.quit();
+                                });
                             });
-
-                            // ------------- Step 4 -------------
-                            //upd.runInstall(newAppPath, [upd.getAppPath(), upd.getAppExec()],{});
-                            //upd.install($dirPath,function(error){
-                            //    console.log("install...");
-                            //    console.log(error);
-                            //    //gui.App.quit();
-                            //    //updater.run(upd.getAppPath()+"\\maxim-test\\Maxim.exe");
-                            //});
                         }
                     }, manifest);
                 }
