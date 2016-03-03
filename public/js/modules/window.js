621fe960c8adae0b2b0ad9d9cee44d8e8999f856
Owenhong <520UED.com@gmail.com>
2016-03-03 20:22:11 +0800

    兼容mac
    1、判断系统，根据系统是否显示自定义关闭、全屏等功能

--- public/js/modules/window.js
+++ public/js/modules/window.js
@@ -31,6 +31,11 @@ define(function(require, exports, module) {
 
 
         //TODO 全局控制窗口开关
+        var isMac = (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel");
+        if(isMac){
+            $(".global-operations a").hide();
+        }
+
         $(".global-operations .close-btn").click(function(){
             win.close();
         });
@@ -56,7 +61,7 @@ define(function(require, exports, module) {
 
         //TODO dialog config
         var $DialogConfig = {
-            frame:false,
+            frame:true,
             toolbar:false,
             position: 'center',
             height:500,
