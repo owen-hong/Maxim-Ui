5747e9bf96e8cf089239ca3f2b4908a2f2415f04
UE <520UED.com@gmail.com>
2016-08-26 10:41:21 +0800

    完善路由注释
    完善路由注释

--- routes.js
+++ routes.js
@@ -6,18 +6,26 @@ var Home = require('./controllers/index');
 
 
 exports.handle = function (app) {
-
+    //index
     app.get('/', Home.index);
+
+    //config.js
     app.get('/config.js', Home.configData);
 
+    //delete poroject
     app.get('/deleteProject', Home.deleteProject);
 
-
+    //update right panel
     app.post('/updateCssSprite', Home.updateCssSprite);
+
+    //upload file
     app.post('/tools/doUploader', Home.doUploader);
 
 
+    //validate ftp fields
     app.get('/validateFtp', Home.validateFtp);
+
+    //edit config.js
     app.post('/tools/doConfig', Home.doConfig);
 }
 
